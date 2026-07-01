6ab24cdac776d42dc956767c23e8f0f4b8f03c7c
raniaa-0 <mastarirania83@gmail.com>
2026-07-01 16:35:33 +0100

    ok

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,13 @@ if (!fs.existsSync(uploadsDir)) {
 const upload = multer({ dest: uploadsDir });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://snapcook-five.vercel.app/'  // ← her actual Vercel URL
+  ],
+  credentials: true
+}));
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
